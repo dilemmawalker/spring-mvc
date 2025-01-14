db296ccb6df45597ee67297b26de3dd59ca5548b
Yash Handa <yashhanda500@gmail.com>
2025-01-14 15:15:51 +0530

    added countries dropbox

--- src/main/java/com/dilemmawalker/thymeleafdemo/work/model/Student.java
+++ src/main/java/com/dilemmawalker/thymeleafdemo/work/model/Student.java
@@ -1,16 +1,15 @@
 package com.dilemmawalker.thymeleafdemo.work.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Setter
 @Getter
 public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
 }
