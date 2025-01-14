c411d95949303cd32fd215eb1a8d5e29224835f9
Yash Handa <yashhanda500@gmail.com>
2025-01-14 15:30:50 +0530

    Updated countries input from application.properties file instead of hardcoding

--- src/main/java/com/dilemmawalker/thymeleafdemo/work/controller/StudentController.java
+++ src/main/java/com/dilemmawalker/thymeleafdemo/work/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.dilemmawalker.thymeleafdemo.work.controller;
 
 import com.dilemmawalker.thymeleafdemo.work.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model){
@@ -19,6 +25,9 @@ public class StudentController {
 
         //add student object to model
         model.addAttribute("student", student);
+
+        //add list of countries to model
+        model.addAttribute("countries", countries);
 
         return "student-form";
     }
