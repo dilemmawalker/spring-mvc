18c92ca1979c2a8d5d2411b6c2923113eb1148d4
Yash Handa <yashhanda500@gmail.com>
2025-01-15 15:41:13 +0530

    added postal codes

--- springMVCValidation/src/main/java/com/dilemmawalker/springMVCValidation/model/Customer.java
+++ springMVCValidation/src/main/java/com/dilemmawalker/springMVCValidation/model/Customer.java
@@ -1,9 +1,6 @@
 package com.dilemmawalker.springMVCValidation.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,4 +18,7 @@ public class Customer {
     @Min(value=0, message="must be greater than or equal to 0")
     @Max(value=10, message="must be less than or equal to 10")
     private int freePass;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message="only 5 chars/digits allowed")
+    private String postalCode;
 }
